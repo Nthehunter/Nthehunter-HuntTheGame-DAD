7af0487750ecfd841e7f4ccd5547d1ecc0a506a4
Nthehunter <71507851+Nthehunter@users.noreply.github.com>
2021-10-16 14:04:18 +0100

    Update db_task_user.sql

--- BackendDatabase/database/databaseHTG/db_task_user.sql
+++ BackendDatabase/database/databaseHTG/db_task_user.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `user`
 --
 
+use db_task;
+
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
